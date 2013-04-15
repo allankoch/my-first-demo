94f41763e801963df50d33f7899c0b18526c2e00
allankoch <THEraven900>
2013-04-15 14:41:21 +0200

    test

--- GitTestApp/Form1.Designer.cs
+++ GitTestApp/Form1.Designer.cs
@@ -33,7 +33,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(62, 46);
+            this.button1.Location = new System.Drawing.Point(104, 93);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 0;
